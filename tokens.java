c663a796d58adea8d854470d213cdeee0904d40f
alphaCoder-69 <eldrago2000@gmail.com>
2020-07-01 20:25:09 -0700

    qwadssdrfsdvcxgfg

--- tokens.java
+++ tokens.java
@@ -3,6 +3,19 @@ class tokens{
     public static void main(String argv[]){
         Scanner sr=new Scanner(System.in);
         String sentence=sr.nextLine();
-        
+        String newSentence="";
+        char senArr[]=sentence.toCharArray();
+        for(int i=0;i<sentence.length();i++){
+           if(senArr[i]==' ' || senArr[i]=='!' || senArr[i]==',' || senArr[i]=='?' || senArr[i]=='.' || senArr[i]=='_' || senArr[i]=='\'' || senArr[i]=='@'){
+               senArr[i]='%';
+           }
+           newSentence=newSentence+senArr[i];
+        }
+        String tokenArr[]=newSentence.split("%");
+        int size=tokenArr.length;
+        System.out.println(size);
+        for(int i=0;i<size;i++){
+            System.out.println(tokenArr[i]);
+        }
     }
 }
