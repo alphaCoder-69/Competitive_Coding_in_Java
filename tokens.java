3fcc505aa473b458cc7794be3dc115ce72eec2db
alphaCoder-69 <eldrago2000@gmail.com>
2020-07-02 13:27:05 -0700

    qew45rdthu

--- tokens.java
+++ tokens.java
@@ -2,14 +2,21 @@ import java.util.*;
 class tokens{
     public static void main(String argv[]){
         Scanner sr=new Scanner(System.in);
+        int j=0;
         String sentence=sr.nextLine();
         String newSentence="";
         char senArr[]=sentence.toCharArray();
         for(int i=0;i<sentence.length();i++){
-           if(senArr[i]==' ' || senArr[i]=='!' || senArr[i]==',' || senArr[i]=='?' || senArr[i]=='.' || senArr[i]=='_' || senArr[i]=='\'' || senArr[i]=='@'){
-               senArr[i]='%';
-           }
-           newSentence=newSentence+senArr[i];
+            if(senArr[i]==' ' || senArr[i]=='!' || senArr[i]==',' || senArr[i]=='?' || senArr[i]=='.' || senArr[i]=='_' || senArr[i]=='\'' || senArr[i]=='@'){
+                if(i-j!=1){
+                    senArr[i]='%';
+                    j=i;
+                }
+                else{
+                    continue;
+                }
+            }
+           newSentence=newSentence+senArr[i]; 
         }
         String tokenArr[]=newSentence.split("%");
         int size=tokenArr.length;
