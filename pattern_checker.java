366fc31f0dd8e82e6bff10ea8ee91ab9f373c7a4
alphaCoder-69 <eldrago2000@gmail.com>
2020-07-16 19:35:35 -0700

    rb96yhzp

--- pattern_checker.java
+++ pattern_checker.java
@@ -1,20 +1,16 @@
 import java.util.*;
+import java.util.regex.*;
 class pattern_checker{
-
-    public static boolean identify(String pattern){
-        String regex="^([a-zA-Z_$][a-zA-Z\\d_$]*)$";
-        Pattern ptn= Pattern.compile(regex);
-        if(pattern==null){return false;}
-        Matcher m=ptn.Matcher(pattern);
-        return m.matches();
-    }
-
     public static void main(String argv[]){
         Scanner sr=new Scanner(System.in);
         int test=sr.nextInt();
+        sr.nextLine();
         for(int i=0;i<test;i++){
-            String patrn=sr.nextLine();
-            identify(patrn);
+            String regex=sr.nextLine();
+            Pattern pattern= Pattern.compile(regex);
+            //if(pattern==null){System.out.println("false");}
+            boolean m=pattern.matcher(regex).matches();
+            System.out.println(m);
         }
     }
 }
